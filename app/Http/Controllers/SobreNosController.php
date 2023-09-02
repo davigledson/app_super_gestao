730989005aa0544f9080168873af3081ff3ef9aa
Davi Gledson <davigledson@outlook.com>
2023-09-02 18:31:28 -0300

    rotas com laravel

---
+++ app/Http/Controllers/SobreNosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class SobreNosController extends Controller
+{
+    public function SobreNos(){
+        echo 'Sobre Nos';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,15 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('seja-bem-vindo');
-});
+// Route::get('/', function () {
+//     return view('seja-bem-vindo');
+// });
+$PrincialControler = new PrincipalController();
+Route::get('/',[$PrincialControler::class,'Principal']);
 
-Route::get('/sobre-nos', function () {
-    return view('sobre-nos');
-});
+Route::get('/sobre-nos', [SobreNosController::class,'SobreNos']);
 
 
-Route::get('/contato', function () {
-    return view('contato');
-});
+Route::get('/contato', [ContatoController::class,'Contato']);
