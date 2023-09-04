9c94a93c1e09bcbf89c3744428943740d2f670cb
Davi Gledson <davigledson@outlook.com>
2023-09-04 03:07:33 -0300

    switch e case com blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -9,8 +9,27 @@ class FornecedorController extends Controller
 {
     public function index(){
         $fornecedores =[
-            0 => ['nome'=>'Fornecedor 1', 'status'=>'N','cnpj'=>'0'],
-            1 => ['nome'=>'Fornecedor 2', 'status'=>'S','cnpj'=>'0sdfsdff'],
+            0 => [
+            'nome'=>'Fornecedor 1',
+             'status'=>'N'
+             ,'cnpj'=>'0',
+             'ddd'=>'11',
+             'telefone'=>'0000-0000'
+            ],
+            1 => [
+                'nome'=>'Fornecedor 2',
+             'status'=>'S',
+             'cnpj'=>'0sdfsdff',
+             'ddd'=>'84',
+             'telefone'=>'0000-0000'
+            ],
+            2 => [
+                'nome'=>'Fornecedor 3',
+             'status'=>'S',
+             'cnpj'=>'0sdfsdff',
+             'ddd'=>'',
+             'telefone'=>'0000-0000'
+            ],
       
     ];
         return view('app.fornecedor.index',compact('fornecedores'));
--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -16,6 +16,28 @@ Fornecedor {{$fornecedores[1]['nome']}}
 <br>
 Status {{$fornecedores[1]['status']}}
 CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Vazio'  }}
+Telefone: {{ $fornecedores[1]['ddd'] ?? 'Vazio'  }} {{ $fornecedores[1]['telefone'] ?? 'Vazio'  }}
+
+@switch ($fornecedores[2]['ddd'])
+    @case('11')
+        Sao paulo - SP
+        @break
+    @case('32')
+    Juiz de fora -MG
+        @break
+
+     @case('84')
+    Barauna-RN
+        @break
+
+    @default
+    Estado não identificado
+
+
+
+
+
+@endswitch
 @endisset
 
 
