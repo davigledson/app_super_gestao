18a73803f54a1cb2a3bdf28a4fbce7d32d868841
Davi Gledson <davigledson@outlook.com>
2023-09-07 01:04:22 -0300

    implementando migrations

--- database/migrations/2023_09_06_064130_create_site_contatos_table.php
+++ database/migrations/2023_09_06_064130_create_site_contatos_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('site_contatos', function (Blueprint $table) {
             $table->id();
+
             $table->timestamps();
+            $table->string('nome',50) ;
+            $table->string('telefone',20) ;
+            $table->string('email',80) ;
+            $table->integer('motivo',50) ;
+            $table->text('mensagem') ;
         });
     }
 
