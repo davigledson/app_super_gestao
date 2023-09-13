9c957434ca1faa9101921759ea15864e1a48d444
Davi Gledson <davigledson@outlook.com>
2023-09-13 19:30:02 -0300

    seeder part 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Fornecedor;
+use App\Models\SiteContato;
 use Illuminate\Database\Seeder;
 
 
@@ -22,6 +23,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(FornecedorSeeder::class);
+        $this->call(SiteContatoSeeder::class);
 
     }
 }
---
+++ database/seeders/SiteContatoSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\SiteContato;
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $contato = new SiteContato();
+        $contato->nome ='Sistema SG';
+        $contato->telefone =' 84 97041665';
+        $contato->email ='contato@gmail.com';
+        $contato->motivo= 1;
+        $contato->mensagem ='Sejá ben-vindo ao sistema';
+        $contato->save();
+    }
+}
