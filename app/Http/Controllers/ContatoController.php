bb9fd9a87f4d36653f6778b88840e0f8f51d06ab
Davi Gledson <davigledson@outlook.com>
2023-09-14 19:04:45 -0300

    inserido dados no db

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -4,11 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\SiteContato;
 class ContatoController extends Controller
 {
-    public function Contato(){
-        var_dump($_POST);
+    public function Contato(Request $request){
+        //dd($request);
+        //var_dump($_POST);
+
+        //TEM ESSA FORMA DE FAZER
+
+        $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo = $request->input('motivo');
+        // $contato->mensagem = $request->input('mensagem');
+
+        //TEM ESSA MAIS ECONOMICA (FILL)
+        // $contato->fill($request->all());
+        // $contato->save();
+
+        //TEM PELO METODO CREATE
+        $contato->create($request->all());
+        //$contato->save();
+
+         print_r($contato->getAttributes());
         return view('site.contato',['titulo'=>'Contato (teste)']); //site/principal - tbm dar certo
     }
 
